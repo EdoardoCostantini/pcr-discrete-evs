38b3da5f9102cc2a33f82dd260f9166e617f10e5
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-05-03 10:56:06 +0200

    refactor: harmonize naming of arguments for correctClass.R

--- code/functions/correctClass.R
+++ code/functions/correctClass.R
@@ -2,18 +2,18 @@
 # Objective: Function to compute the classificaiton error
 # Author:    Edoardo Costantini
 # Created:   2022-04-29
-# Modified:  2022-04-29
+# Modified:  2022-05-03
 
-correctClass <- function(y_true, preds) {
+correctClass <- function(y_true, y_hat) {
   # Internals -------------------------------------------------------------
 
-  # preds = factor(c(1, 1, 0, 1, 2, 1, 3, 0, 1)) # factor of predictions
+  # y_hat = factor(c(1, 1, 0, 1, 2, 1, 3, 0, 1)) # factor of predictions
   # y_true = factor(c(0, 1, 0, 1, 1, 1, 3, 2, 1)) # factor of observed values
 
   # Body ------------------------------------------------------------------
 
   # Compute proportion of correctly classified cases
-  prop_cclass <- mean(preds == y_true)
+  prop_cclass <- mean(y_hat == y_true)
 
   # Return it
   return(prop_cclass)
