a7159dcbcf4a5334de4aa17e16ac146decdb5f42
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-05-16 14:30:10 +0200

    fix: add a correction for predicted 0 probability

--- code/functions/crossEntropy.R
+++ code/functions/crossEntropy.R
@@ -2,7 +2,7 @@
 # Objective: function to compute corss-entropy
 # Author:    Edoardo Costantini
 # Created:   2022-04-22
-# Modified:  2022-04-22
+# Modified:  2022-05-16
 
 crossEntropy <- function(p = matrix(), p_hat = matrix()) {
   # Internals -------------------------------------------------------------
@@ -11,6 +11,16 @@ crossEntropy <- function(p = matrix(), p_hat = matrix()) {
   # p_hat = matrix(c(.9, .1, .8, .2, .8, .24, .12)) # predicted probability value
 
   # Body ------------------------------------------------------------------
+
+  # Correction for perfect 0 probability predictions
+  eps <- 1e-15
+  p_hat <- t(
+    apply(p_hat, 1, function (r){
+      pmax(r, eps)
+    })
+  )
+
+  # Compute CE
   ce <- -sum(diag(p %*% t(log(p_hat))))
 
   # Return
